c6f2a073e17b634531dc239cb799da507756d67e
ElectroMechByte <panchakarla.kiran@gmail.com>
2023-09-26 18:07:37 +0530

    Update excel.js

--- excel.js
+++ excel.js
@@ -135,7 +135,7 @@
         onCustomWidgetAfterUpdate(changedProperties) {
             var that = this;
 
-            let xlsxjs = "http://localhost/SAC/sacexcel/xlsx.js";
+            let xlsxjs = "https://electromechbyte.github.io/newGitTest/xlsx.js";
             async function LoadLibs() {
                 try {
                     await loadScript(xlsxjs, _shadowRoot);
